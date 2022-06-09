5bcdeceb45fbd883f6cf6f0869a4748218743c1c
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-09 15:17:09 -0500

    3:17

--- springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
+++ springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.cogent.springsecurityDemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import com.cogent.springsecurityDemo.security.service.UserDetailsServiceImpl;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
+@Autowired
+UserDetailsServiceImpl userDetailsService;
+@
+private
 }
