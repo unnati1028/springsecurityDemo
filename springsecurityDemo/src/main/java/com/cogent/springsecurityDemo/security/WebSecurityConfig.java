cfa8bc90ee496189567760389b9bad4036a6b59f
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-09 15:54:31 -0500

    3:54

--- springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
+++ springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.cogent.springsecurityDemo.security.jwt.AuthTokenFilter;
 import com.cogent.springsecurityDemo.security.service.UserDetailsServiceImpl;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
@@ -21,4 +26,18 @@ private
 public AuthTokenFilter authenticationJwtTokenFilter() {
 	return new AuthTokenFilter();
 }
+protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+	auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+}
+@Bean
+@Override
+public AuthenticationManager authenticationManagerBean() throws Exception {
+  return super.authenticationManagerBean();
+}
+
+@Bean
+public PasswordEncoder passwordEncoder() {
+  return new BCryptPasswordEncoder();
+}
+
 }
