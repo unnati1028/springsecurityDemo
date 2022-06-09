1df3ca2d8db0f68d222bca3d674183edf9d2382a
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-09 15:38:24 -0500

    3:38

--- springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
+++ springsecurityDemo/src/main/java/com/cogent/springsecurityDemo/security/WebSecurityConfig.java
@@ -1,15 +1,24 @@
 package com.cogent.springsecurityDemo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.cogent.springsecurityDemo.security.jwt.AuthTokenFilter;
 import com.cogent.springsecurityDemo.security.service.UserDetailsServiceImpl;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableGlobalMethodSecurity;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 @Autowired
 UserDetailsServiceImpl userDetailsService;
-@
+@Autowired
 private
+@Bean
+public AuthTokenFilter authenticationJwtTokenFilter() {
+	return new AuthTokenFilter();
+}
 }
